f4bf08cf3f386aafeaafd5e96e6496d53917de34
SungIn-Shin <tlstjddls123@naver.com>
2023-05-04 12:25:55 +0900

    travis ci test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>srcasdasasdfasasdfasdfdd/App.js</code> and save toasdfasdf relasdasdoad. Learn React 안녕하세요asdfsadfasdf. 반갑습니sadfasdfasdfasdf다 gdgd
+          Edit <code>srcasdasasdfasasdfasdfdd/App.js</code>push를 위한 테스트 and save toasdfasdf relasdasdoad. Learn React 안녕하세요asdfsadfasdf. 반갑습니sadfasdfasdfasdf다 gdgd
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"></a>
       </header>
